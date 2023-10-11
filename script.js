63641275bf86daf4ceed9b6e4910c0e662c3b4ef
TomDeeterink1 <deeterinktom@gmail.com>
2023-10-11 14:37:21 +0200

    commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('.hamburger');
+    const hiddencontent = document.querySelector('.hidden');
+  
+    button.addEventListener('click', dropdown);
+  
+    function dropdown() {
+      hiddencontent.classList.toggle('visible');
+    }
+  });
