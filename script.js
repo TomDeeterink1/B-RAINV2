b234819df4e58508a09c6e3d89db3df883bb1293
TomDeeterink1 <deeterinktom@gmail.com>
2023-10-11 18:15:16 +0200

    L

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+
+
+// Voeg een klikgebeurtenis toe aan de cirkels (containers)
+const containers = document.querySelectorAll('.container');
+containers.forEach(container => {
+    container.addEventListener('click', () => {
+        // Verander de zichtbaarheid van de bijbehorende content
+        const content = container.querySelector('.content');
+        if (content.style.visibility === 'hidden' || content.style.visibility === '') {
+            content.style.visibility = 'visible';
+        } else {
+            content.style.visibility = 'hidden';
+        }
+    });
+});
+
+
 document.addEventListener('DOMContentLoaded', function () {
-    const button = document.querySelector('.hamburger');
-    const hiddencontent = document.querySelector('.hidden');
-  
-    button.addEventListener('click', dropdown);
-  
-    function dropdown() {
-      hiddencontent.classList.toggle('visible');
-    }
-  });
+  const button = document.querySelector('.hamburger');
+  const hiddencontent = document.querySelector('.hidden');
+
+  button.addEventListener('click', dropdown);
+
+  function dropdown() {
+    hiddencontent.classList.toggle('visible');
+  }
+});
