2d6a42d5cd1200394f7abba662d226cd161a33e2
TomDeeterink1 <deeterinktom@gmail.com>
2023-10-12 17:18:58 +0200

    big push

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-
-
-// Voeg een klikgebeurtenis toe aan de cirkels (containers)
-const containers = document.querySelectorAll('.container');
-containers.forEach(container => {
-    container.addEventListener('click', () => {
-        // Verander de zichtbaarheid van de bijbehorende content
-        const content = container.querySelector('.content');
-        if (content.style.visibility === 'hidden' || content.style.visibility === '') {
-            content.style.visibility = 'visible';
-        } else {
-            content.style.visibility = 'hidden';
-        }
-    });
-});
-
-
 document.addEventListener('DOMContentLoaded', function () {
   const button = document.querySelector('.hamburger');
   const hiddencontent = document.querySelector('.hidden');
@@ -22,6 +5,24 @@ document.addEventListener('DOMContentLoaded', function () {
   button.addEventListener('click', dropdown);
 
   function dropdown() {
-    hiddencontent.classList.toggle('visible');
+    hiddencontent.classList.toggle('.visible');
   }
 });
+
+{
+  const containers = document.querySelectorAll('.container');
+  containers.forEach(container => {
+      container.addEventListener('click', () => {
+          const content = container.querySelector('.content');
+          if (content.style.visibility === 'hidden' || content.style.visibility === '') {
+              content.style.visibility = 'visible';
+          } else {
+              content.style.visibility = 'hidden';
+          }
+      });
+  });
+}
+
+
+ 
+
